5ed31cd67027967cdf4836a62d779d54e34c6597
eminekibar <eminekibar.2834@gmail.com>
2025-02-16 21:05:13 +0300

    serverclient

--- minitalk/client.c
+++ minitalk/client.c
@@ -1,45 +1,67 @@
+GNU nano 7.2                                                                                          client.c                                                                                                   /* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekibar <ekibar@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/15 14:20:45 by ekibar          #+#    #+#             */
+/*   Updated: 2025/02/15 14:20:46 by ekibar         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
 void    send_message(char a, int server_pid)
 {
-    int i;
+        int     i;
 
-    i = 0;
-    while(i < 8)
-    {
-        if((a << i) & 128)
-            kill(server_pid, SIGUSR1);
-        else
-            kill(server_pid, SIGUSR2);
-    
-        i++;
-        usleep(100);
-    }
+        i = 0;
+        while (i < 8)
+        {
+                if ((a << i) & 128)
+                {
+                        if (kill(server_pid, SIGUSR1) == -1)
+                        {
+                                ft_printf("Error: enter correct pid\n");
+                                exit(1);
+                        }
+                }
+                else
+                {
+                        if (kill(server_pid, SIGUSR2) == -1)
+                        {
+                                ft_printf("Error: enter correct pid\n");
+                                exit(1);
+                        }
+                }
+                i++;
+                usleep(100);
+        }
 }
 
-int main(int argc, char *argv[])
+int     main(int argc, char *argv[])
 {
-    int server_pid;
-    int i;
+    int     server_pid;
+    int     i;
 
+    server_pid = -1;
     i = 0;
-    if(argc == 3)
+    if (argc == 3)
     {
-        server_pid = ft_atoi(argv[1]);
-        if (server_pid != true_pid())
-        {
-            ft_printf("Hata: Geçersiz PID!\n");
-            return (1);
-        }
-        while(argv[2][i])
-        {
-            send_message(argv[2][i], server_pid);
-            i++;
-        }
+    server_pid = ft_atoi(argv[1]);
+    if (server_pid <= 0)
+    {
+        ft_printf("Error: Geçersiz PID\n");
+        return (1);
+    }
+    while (argv[2][i])
+    {
+        send_message(argv[2][i], server_pid);
+        i++;
+    }
     }
     else
-    {
-        ft_printf("Error: %s <Server_PID> <Message>\n", argv[0]);
-        exit (0);
-    }
+        ft_printf("Error: %s <Server_Pid> <Message>\n", argv[0]);
+    return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -1,46 +1,60 @@
+GNU nano 7.2                                                                                                                        server.c                                                                                                                                 /* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekibar <ekibar@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/15 14:20:45 by ekibar          #+#    #+#             */
+/*   Updated: 2025/02/15 14:20:46 by ekibar         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
-void write_message(int signal_type)
+void    write_message(int signal_type)
 {
-    static int i;
-    static char a;
+        static int      i;
+        static char     a;
 
-    if(signal_type == SIGUSR1)
-        a = (a << 1) | 1;
-    else if(signal_type == SIGUSR2)
-        a = (a << 1);
-    else
-        return;
-    i++;
-    if(i == 8)
-    {
-        ft_printf("%c", a);
-        i = 0;
-        a = 0;
-    }
+        a = a << 1;
+        if (signal_type == SIGUSR1)
+                a = a | 1;
+        else if (signal_type == SIGUSR2)
+                a = a | 0;
+        else
+                return;
+        if (i == 7)
+                ft_printf("%c", a);
+        i++;
+        if (i == 8)
+        {
+                a = 0;
+                i = 0;
+        }
 }
 
-int main()
+int     main()
 {
-    int     server_pid;
+        int     server_pid;
 
-    server_pid = getpid();
-    if (server_pid <= 0)
-    {
-        ft_printf("Error: PID alınamadı!\n");
-        return (1);
-    }
-    ft_printf("Server PID: %d\n", server_pid);
-    if (signal(SIGUSR1, write_message) == SIG_ERR)
-    {
-        ft_printf("Error: SIGUSR1 sinyali ayarlanamadı!\n");
-        return (1);
-    }
-    if (signal(SIGUSR2, write_message) == SIG_ERR)
-    {
-        ft_printf("Error: SIGUSR2 sinyali ayarlanamadı!\n");
-        return (1);
-    }
-    while(1)
-        pause();
+        server_pid = getpid();
+        if (server_pid <= 0)
+        {
+                ft_printf("Error: PID alınamadı!\n");
+                return (1);
+        }
+        ft_printf("Server PID: %d\n", server_pid);
+        if (signal(SIGUSR1, write_message) == SIG_ERR)
+        {
+                ft_printf("Error: SIGUSR1 sinyali ayarlanamadı!\n");
+                return (1);
+        }
+        if (signal(SIGUSR2, write_message) == SIG_ERR)
+        {
+                ft_printf("Error: SIGUSR2 sinyali ayarlanamadı!\n");
+                return (1);
+        }
+        while(1)
+                pause();
 }
