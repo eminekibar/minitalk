4c58d5d86d07797000aaf67eab38b5b69d3619f6
Emine <emine@DESKTOP-OVUAG83.>
2025-02-18 15:10:37 +0300

    minitalk

--- minitalk/client.c
+++ minitalk/client.c
@@ -12,56 +12,56 @@
 
 #include "minitalk.h"
 
-void    send_message(char a, int server_pid)
+void	send_message(char a, int server_pid)
 {
-    int     i;
+	int	i;
 
-    i = 0;
-    while (i < 8)
-    {
-        if ((a << i) & 128)
-        {
-            if (kill(server_pid, SIGUSR1) == -1)
-            {
-                ft_printf("Error: enter correct pid\n");
-                exit(1);
-            }
-        }
-        else
-        {
-            if (kill(server_pid, SIGUSR2) == -1)
-            {
-                ft_printf("Error: enter correct pid\n");
-                exit(1);
-            }
-        }
-        i++;
-        usleep(100);
-    }
+	i = 0;
+	while (i < 8)
+	{
+		if ((a << i) & 128)
+		{
+			if (kill(server_pid, SIGUSR1) == -1)
+			{
+				ft_printf("Error: Uncorrect PID\n");
+				exit(1);
+			}
+		}
+		else
+		{
+			if (kill(server_pid, SIGUSR2) == -1)
+			{
+				ft_printf("Error: Uncorrect PID\n");
+				exit(1);
+			}
+		}
+		i++;
+		usleep(100);
+	}
 }
 
-int     main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    int     server_pid;
-    int     i;
+	int	server_pid;
+	int	i;
 
-    server_pid = -1;
-    i = 0;
-    if (argc == 3)
-    {
-        server_pid = ft_atoi(argv[1]);
-        if (server_pid <= 0)
-        {
-            ft_printf("Error: Geçersiz PID\n");
-            return (1);
-        }
-        while (argv[2][i])
-        {
-            send_message(argv[2][i], server_pid);
-            i++;
-        }
-    }
-    else
-        ft_printf("Error: %s <Server_Pid> <Message>\n", argv[0]);
-    return (0);
+	server_pid = -1;
+	i = 0;
+	if (argc == 3)
+	{
+		server_pid = ft_atoi(argv[1]);
+		if (server_pid <= 0)
+		{
+			ft_printf("Error: Invalid PID\n");
+			exit (1);
+		}
+		while (argv[2][i])
+		{
+			send_message(argv[2][i], server_pid);
+			i++;
+		}
+	}
+	else
+		ft_printf("Error: %s <Server_Pid> <Message>\n", argv[0]);
+	return (0);
 }
--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekibar <ekibar@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/15 14:20:45 by ekibar          #+#    #+#             */
+/*   Updated: 2025/02/15 14:20:46 by ekibar         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
-#define MINITALK_H
+# define MINITALK_H
 
-#include <unistd.h>
-#include <signal.h>
-#include <stdarg.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <stdarg.h>
+# include <stdlib.h>
 
-int     ft_printf(const char *s, ...);
-int     ft_atoi(const char *str);
+int	ft_printf(const char *s, ...);
+int	ft_atoi(const char *str);
 
 #endif
